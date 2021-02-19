49d31fcc46f2e33800c7f1d9e5880d278c020591
Adam Honore <61853978+ahonore42@users.noreply.github.com>
2021-02-19 12:39:55 -0600

    Update PostCard.js

--- starter-code/src/components/PostCard.js
+++ starter-code/src/components/PostCard.js
@@ -3,10 +3,10 @@ import React, {Component} from 'react'
 export default class PostCard extends Component {
   render() {
     return (
-      <div className="item" key={props.id}>
-        <img src={props.image} alt="Post banner" />
+      <div className="item" key={this.props.id}>
+        <img src={this.props.image} alt="Post banner" />
         <div className="modal">
-          <h3>{props.title}</h3>
+          <h3>{this.props.title}</h3>
           <button>Read More</button>
         </div>
       </div>
