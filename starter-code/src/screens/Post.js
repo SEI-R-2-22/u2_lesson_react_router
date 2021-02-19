3ed43af11366f12f57f96a9de34547404015e376
Adam Honore <61853978+ahonore42@users.noreply.github.com>
2021-02-19 12:30:18 -0600

    refactor Post to Class component

--- starter-code/src/screens/Post.js
+++ starter-code/src/screens/Post.js
@@ -1,21 +1,21 @@
-import React from 'react'
+import React, {Component} from 'react'
 import data from '../FakeData.json'
-const Post = (props) => {
-  // console.table(props)
-  const post = data.find(
-    (post) => post.id === parseInt(props.match.params.post_id)
-  )
 
-  return (
-    <div className="post">
-      <div className="hero">
-        <img src={post.image} alt="post banner" />
+export default class Post extends Component {
+  render() {
+    const post = data.find((post) => post.id === parseInt(this.props.match.params.post_id));
+    
+    return (
+      <div className="post">
+        <div className="hero">
+          <img src={post.image} alt="post banner" />
+        </div>
+        <div className="content">
+          <h3>{post.title}</h3>
+          <p>{post.body}</p>
+        </div>
       </div>
-      <div className="content">
-        <h3>{post.title}</h3>
-        <p>{post.body}</p>
-      </div>
-    </div>
-  )
+    );
+  }
 }
-export default Post
+
