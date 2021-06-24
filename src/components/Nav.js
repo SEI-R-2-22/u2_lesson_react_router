ec17d129165190ed806cb217d1134a729ba08b1e
Andre Pato <anpato1994@gmail.com>
2021-06-24 08:25:23 -0400

    migrate codebase to hooks

---
+++ src/components/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <nav className="navbar">
+      <h4>Starboard</h4>
+      <div>{/* create NavLinks here */}</div>
+    </nav>
+  )
+}
+
+export default Nav
