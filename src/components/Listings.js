760bc1a5260ab304fefb5bc50ddbfc933aa17d81
Michael Lackey <59581978+mlackey9601@users.noreply.github.com>
2021-12-08 19:54:48 -0600

    Update Listings.js

--- src/components/Listings.js
+++ src/components/Listings.js
@@ -10,7 +10,7 @@ const Listings = (props) => {
     <div className="boat-grid">
       {
       props.boats.map((boat) => (
-        <div className="boat-card" onClick={() => showBoat(boat)}>
+        <div className="boat-card" onClick={() => showBoat(boat)} key={boat.id}>
           <img style={{ display: 'block' }} src={boat.img} alt={boat.name} />
           <h3>{boat.name}</h3>
         </div>
