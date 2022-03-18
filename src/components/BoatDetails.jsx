9e24426343a3ff774637957c755b0ff0e120a1e0
Michael Lackey <michael.g.lackey@gmail.com>
2022-03-18 10:50:09 -0500

    update starter

--- src/components/BoatDetails.jsx
+++ src/components/BoatDetails.jsx
@@ -1,27 +1,27 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react'
 
 const BoatDetails = (props) => {
 
-  const [selectedBoat, setBoat] = useState('')
+  const [boat, setBoat] = useState('')
 
   useEffect(() => {
     
   }, [])
 
-  return selectedBoat ? (
+  return boat ? (
     <div className="detail">
       <div className="detail-header">
-        <img src={selectedBoat.img} alt={selectedBoat.name} />
+        <img src={boat.img} alt={boat.name} />
         <div style={{minWidth: '30em', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-          <h1>{selectedBoat.name}</h1>
+          <h1>{boat.name}</h1>
         </div> 
       </div>
       <div className="info-wrapper">
         <div style={{display: 'flex', justifyContent: 'space-between'}}>
-          <h3>Price: ${selectedBoat.price}</h3>
-          <h3>Boat ID: {selectedBoat.id}</h3>
+          <h3>Price: ${boat.price}</h3>
+          <h3>Boat ID: {boat.id}</h3>
         </div>
-        <p>{selectedBoat.description}</p>
+        <p>{boat.description}</p>
       </div>
     </div>
   ) : null;
